5e50fc2f63f779d7f24d174955b60b719d9354f8
Steopik <kamushev.05@gmail.com>
2025-05-18 15:29:01 +0300

    add Infrastructure

---
+++ Lr3/StudentManager/Infrastructure/Adapters/QuoteApiAdapter.cs
@@ -0,0 +1,33 @@
+﻿using StudentManager.Application.Interfaces;
+using System.Text.Json;
+
+namespace StudentManager.Infrastructure.Adapters;
+
+public class QuoteApiAdapter : IQuoteService
+{
+    private readonly HttpClient _httpClient;
+
+    public QuoteApiAdapter()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public string GetMotivationalQuote()
+    {
+        try
+        {
+            var response = _httpClient.GetAsync("https://zenquotes.io/api/random").Result;
+            response.EnsureSuccessStatusCode();
+
+            var json = response.Content.ReadAsStringAsync().Result;
+            var doc = JsonDocument.Parse(json);
+
+            var quote = doc.RootElement[0].GetProperty("q").GetString();
+            return $"{quote}";
+        }
+        catch
+        {
+            return "Не удалось получить цитату.";
+        }
+    }
+}
---
+++ Lr3/StudentManager/Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,52 @@
+﻿using StudentManager.Domain.Entities;
+using StudentManager.Application.Interfaces;
+using System.Text.Json;
+
+namespace StudentManager.Infrastructure.Repositories;
+
+using System.Text.Json;
+using StudentManager.Domain.Entities;
+
+public class StudentRepository : IStudentRepository
+{
+    private readonly string _filePath = "../../../../students.json";
+
+    public void Add(Student student)
+    {
+        var students = GetAll();
+        students.Add(student);
+        SaveAll(students);
+    }
+
+    public void Update(Student student)
+    {
+        var students = GetAll();
+        var index = students.FindIndex(s => s.Id == student.Id);
+        if (index >= 0)
+        {
+            students[index] = student;
+            SaveAll(students);
+        }
+    }
+
+    public Student? GetById(string id)
+    {
+        var students = GetAll();
+        return students.Find(s => s.Id == id);
+    }
+
+    public List<Student> GetAll()
+    {
+        if (!File.Exists(_filePath))
+            return new List<Student>();
+
+        var json = File.ReadAllText(_filePath);
+        return JsonSerializer.Deserialize<List<Student>>(json) ?? new List<Student>();
+    }
+
+    private void SaveAll(List<Student> students)
+    {
+        var json = JsonSerializer.Serialize(students, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(_filePath, json);
+    }
+}
