b422be1ae499ca89abf351a3b18b5a3a86149d76
Steopik <kamushev.05@gmail.com>
2025-05-18 15:26:54 +0300

    add Domain

---
+++ Lr3/StudentManager/Domain/DTOs/StudentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentManager.Domain.DTOs;
+
+public class StudentDTO
+{
+    public string? Id { get; set; }   
+    public string Name { get; set; } = string.Empty;
+    public int Mark { get; set; }
+}
---
+++ Lr3/StudentManager/Domain/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StudentManager.Domain.Entities;
+
+public class Student
+{
+    public string Id { get; private set; }  
+    public string Name { get; private set; }
+    public int Mark { get; private set; }
+
+    [JsonConstructor]
+    public Student(string id, string name, int mark)
+    {
+        Id = id;
+        Name = name;
+        Mark = mark;
+    }
+
+    public static Student Create(string name, int mark)
+    {
+        return new Student(Guid.NewGuid().ToString(), name, mark);
+    }
+
+    public void Update(string name, int mark)
+    {
+        Name = name;
+        Mark = mark;
+    }
+}
